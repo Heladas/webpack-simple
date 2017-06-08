72d7c70946c9fbda236c738c9d10dbf7a27ee7bd
Heladas <mart-workbox@yandex.ru>
2017-06-08 18:17:01 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "func-names": ["error", "never"]
+    }
+};
